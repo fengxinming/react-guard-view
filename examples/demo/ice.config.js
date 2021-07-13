6e9a5968d1cecd805fba76678f29d7ac81c063a3
Jesse Feng <fxm0016@126.com>
2021-07-13 22:15:32 +0800

    Create ice.config.js

---
+++ examples/demo/ice.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const pkg = require('./package.json');
+
+module.exports = {
+  entry: 'src/index.jsx',
+  filename: `[name].js?v=${pkg.version}`,
+  plugins: [
+    ['ice-plugin-moment-locales', {
+      locales: ['zh-cn']
+    }]
+  ],
+  alias: {
+    '@': path.join(__dirname, './src'),
+    'route-view/es': path.join(__dirname, '../../packages/route-view/src'),
+    'route-view': path.join(__dirname, '../../packages/route-view/src')
+  },
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    publicPath: '/',
+    port: 3456,
+    disableHostCheck: true
+  },
+  chainWebpack(chainedConfig, { command }) {
+    chainedConfig.module
+      .rule('jsx')
+      .use('babel-loader')
+      .tap((options) => {
+        options.plugins.push(
+          ['@babel/plugin-proposal-private-methods', { 'loose': true }],
+          'babel-plugin-jsx-advanced'
+        );
+        return options;
+      });
+
+    chainedConfig.module
+      .rule('swf')
+      .test(/\.swf$/)
+      .use('swf')
+      .loader(require.resolve('file-loader'))
+      .options({
+        name: 'assets/[hash].[ext]'
+      });
+
+    // chainedConfig
+    //   .plugin('HtmlWebpackPlugin')
+    //   .tap((options) => {
+    //     Object.assign(options[0].templateParameters, {
+
+    //     });
+    //     return options;
+    //   });
+
+    if (command === 'build') {
+      chainedConfig.optimization
+        .minimizer('UglifyJsPlugin')
+        .tap((args) => {
+          args[0].uglifyOptions.compress.pure_funcs = ['console.log'];
+          return args;
+        });
+    }
+  }
+};
