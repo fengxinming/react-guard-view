26013a0826451adeb6a0fee352db87af3757fe2d
Jesse Feng <fxm0016@126.com>
2021-07-13 22:10:40 +0800

    Create create.js

---
+++ packages/route-view/src/create.js
@@ -0,0 +1,77 @@
+import { createElement } from 'react';
+import RouteView from './RouteView';
+
+/**
+ * 用于创建连接函数
+ *
+ * @param {object} config
+ * @returns
+ */
+export default function create({
+  beforeEach,
+  afterEach
+} = {}) {
+  let beforeHooks = [];
+  let afterHooks = [];
+
+  if (beforeEach) {
+    beforeHooks = beforeHooks.concat(beforeEach);
+  }
+  if (afterEach) {
+    afterHooks = afterHooks.concat(afterEach);
+  }
+
+  let unlisten;
+  const listen = function (history) {
+    if (!unlisten) {
+      unlisten = history.listen(() => {
+        history.previousRouteInfo = history.currentRouteInfo;
+        history.currentRouteInfo = null;
+      });
+    }
+  };
+
+  return {
+    unlisten() {
+      unlisten && unlisten();
+    },
+
+    /**
+     * 创建 RouteView
+     *
+     * @param {React.Component|React.FC} component
+     * @param {object} config
+     * @returns
+     */
+    connect(component, {
+      beforeEnter,
+      afterEnter
+    }) {
+      beforeEnter = beforeEnter
+        ? beforeHooks.concat(beforeEnter)
+        : beforeHooks;
+
+      afterEnter = afterEnter
+        ? afterHooks.concat(afterEnter)
+        : afterHooks;
+
+      // 如果没有定义钩子函数，就直接渲染组件
+      if (!beforeEnter.length && !afterEnter.length) {
+        return function (propsFromRoute) {
+          listen(propsFromRoute.history);
+          return createElement(component, propsFromRoute);
+        };
+      }
+
+      return function (propsFromRoute) {
+        listen(propsFromRoute.history);
+        return createElement(RouteView, {
+          propsFromRoute: propsFromRoute,
+          component,
+          beforeHooks: beforeEnter,
+          afterHooks: afterEnter
+        });
+      };
+    }
+  };
+}
