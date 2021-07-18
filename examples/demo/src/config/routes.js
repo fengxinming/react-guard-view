11340e2d0d376d6fbc99befa132735af0095de2e
Jesse Feng <fxm0016@126.com>
2021-07-18 14:59:54 +0800

    Update routes.js

--- examples/demo/src/config/routes.js
+++ examples/demo/src/config/routes.js
@@ -1,4 +1,5 @@
-import create from 'react-guard-view/es/create';
+import createRouteView from 'react-guard-view/es/create';
+import { create } from '../common/router';
 import BasicLayout from '../layouts/BasicLayout';
 import Devices from '../pages/devices';
 import Guide from '../pages/guide';
@@ -6,90 +7,96 @@ import Monitor from '../pages/monitor';
 import System from '../pages/system';
 import Tasks from '../pages/tasks';
 import Login from '../pages/login';
-import { create as createRouter } from '../common/router';
+import Inactive from '../pages/inactive';
+import { isActive, isLogin } from '@/common/user';
+// import { show } from '@/common/gloading';
 
-const { connect } = create({
+const { connect } = createRouteView({
   beforeEach(to, from, next) {
     console.info('beforeEach', 'to', to, 'from', from);
-    if (to.name !== 'login') {
-      if (!sessionStorage.getItem('token')) {
-        return next('/login');
-      }
+
+    const { name } = to;
+    if ((name === 'login' && isLogin())
+      || (name === 'inactive' && isActive())) {
+      to.history.push('/');
+      return;
     }
-    else if (sessionStorage.getItem('token')) {
-      return next('/');
-    }
+
     next();
   }
 });
 
-const routes = [
-  {
-    name: 'login',
-    path: '/login',
-    exact: true,
-    component: connect(Login)
-  },
-  {
-    path: '/',
-    component: BasicLayout,
-    children: [
-      {
-        path: '/',
-        redirect: '/guide'
-      },
-      {
-        path: '/guide',
-        render: connect(Guide, {
-          beforeEnter(to, from, next) {
-            console.info('beforeEnter guide');
-            next();
-          }
-        })
-      },
-      {
-        path: '/devices',
-        render: connect(Devices, {
-          beforeEnter(to, from, next) {
-            console.info('beforeEnter devices');
-            next();
-          }
-        })
-      },
-      {
-        path: '/tasks',
-        render: connect(Tasks, {
-          beforeEnter(to, from, next) {
-            console.info('beforeEnter tasks');
-            next('/guide');
-          }
-        })
-      },
-      {
-        path: '/monitor',
-        render: connect(Monitor, {
-          beforeEnter(to, from, next) {
-            console.info('beforeEnter monitor');
-            next();
-          }
-        })
-      },
-      {
-        path: '/system',
-        render: connect(System, {
-          beforeEnter(to, from, next) {
-            console.info('beforeEnter system');
-            next();
-          }
-        })
-      }
-    ]
-  }
-];
-
-const { history, Router } = createRouter({
+const { Router, history } = create({
   mode: 'hash',
-  routes
+  routes: [
+    {
+      name: 'login',
+      path: '/login',
+      exact: true,
+      component: connect(Login)
+    },
+    {
+      name: 'inactive',
+      path: '/inactive',
+      exact: true,
+      component: connect(Inactive)
+    },
+    {
+      path: '/',
+      component: BasicLayout,
+      children: [
+        {
+          path: '/',
+          redirect: '/guide'
+        },
+        {
+          path: '/guide',
+          render: connect(Guide, {
+            beforeEnter(to, from, next) {
+              console.info('beforeEnter guide');
+              next();
+            }
+          })
+        },
+        {
+          path: '/devices',
+          render: connect(Devices, {
+            beforeEnter(to, from, next) {
+              console.info('beforeEnter devices');
+              next();
+            }
+          })
+        },
+        {
+          path: '/tasks',
+          render: connect(Tasks, {
+            beforeEnter(to, from, next) {
+              console.info('beforeEnter tasks');
+              next('/guide');
+            }
+          })
+        },
+        {
+          path: '/monitor',
+          render: connect(Monitor, {
+            beforeEnter(to, from, next) {
+              console.info('beforeEnter monitor');
+              next();
+            }
+          })
+        },
+        {
+          path: '/system',
+          render: connect(System, {
+            beforeEnter(to, from, next) {
+              console.info('beforeEnter system');
+              next();
+            }
+          })
+        }
+      ]
+    }
+  ]
 });
 
-export { history, Router };
+export { Router, history };
